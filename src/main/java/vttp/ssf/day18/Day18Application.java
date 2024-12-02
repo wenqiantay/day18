46085dce9c84b6b0b1ca5d3a75910a2dc539b58e
wenqiantay <wenqianhello@gmail.com>
2024-12-02 09:47:51 +0800

    initial commit

---
+++ src/main/java/vttp/ssf/day18/Day18Application.java
@@ -0,0 +1,13 @@
+package vttp.ssf.day18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day18Application.class, args);
+	}
+
+}
